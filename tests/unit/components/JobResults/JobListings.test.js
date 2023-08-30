632544ebe1da9e767b0b4e59d88b90877650025f
astropsy999 <bareust@gmail.com>
2023-08-30 20:22:40 +0300

    18 dynamic pagination

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -7,25 +7,128 @@ import JobListings from '@/components/JobResults/JobListings.vue'
 vi.mock('axios')
 
 describe('JobListings', () => {
-  it('fetches jobs', () => {
-    axios.get.mockResolvedValue({ data: [] })
-    render(JobListings)
-    expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/jobs')
+  const createRoute = (queryParams = {}) => ({
+    query: {
+      page: '5',
+      ...queryParams
+    }
   })
 
-  it('creates for jobs listing for every job', async () => {
-    axios.get.mockResolvedValue({ data: Array(15).fill({}) })
-
+  const renderJobListings = ($route) => {
     render(JobListings, {
       global: {
+        mocks: {
+          $route
+        },
         stubs: {
           'router-link': RouterLinkStub
         }
       }
     })
+  }
+  it('fetches jobs', () => {
+    axios.get.mockResolvedValue({ data: [] })
+    const $route = createRoute()
+
+    renderJobListings($route)
+
+    expect(axios.get).toHaveBeenCalledWith('http://myfakeapi.com/jobs')
+  })
+
+  it('displays maximum of 10 jobs', async () => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) })
+    const queryParams = { page: '1' }
+    const $route = createRoute(queryParams)
+
+    renderJobListings($route)
 
     const jobListings = await screen.findAllByRole('listitem')
 
-    expect(jobListings).toHaveLength(15)
+    expect(jobListings).toHaveLength(10)
+  })
+
+  describe('when params exclude the page number', () => {
+    it('displays page number 1', () => {
+      const queryParams = { page: undefined }
+      const $route = createRoute(queryParams)
+
+      renderJobListings($route)
+
+      expect(screen.getByText('Page 1')).toBeInTheDocument()
+    })
+  })
+
+  describe('when params include the page number', () => {
+    it('displays page number', () => {
+      const queryParams = { page: '3' }
+      const $route = createRoute(queryParams)
+
+      renderJobListings($route)
+
+      expect(screen.getByText('Page 3')).toBeInTheDocument()
+    })
+  })
+
+  describe('when user is on the first page', () => {
+    it('it does not show link to previous page', async () => {
+      axios.get.mockResolvedValue({ data: Array(15).fill({}) })
+      const queryParams = { page: '1' }
+      const $route = createRoute(queryParams)
+      renderJobListings($route)
+
+      await screen.findAllByRole('listitem')
+
+      const previousLink = screen.queryByRole('link', {
+        name: /previous/i
+      })
+
+      expect(previousLink).not.toBeInTheDocument()
+    })
+
+    it('shows link to the next page', async () => {
+      axios.get.mockResolvedValue({ data: Array(15).fill({}) })
+      const queryParams = { page: '1' }
+      const $route = createRoute(queryParams)
+      renderJobListings($route)
+
+      await screen.findAllByRole('listitem')
+
+      const nextLink = screen.queryByRole('link', {
+        name: /next/i
+      })
+
+      expect(nextLink).toBeInTheDocument()
+    })
+  })
+  describe('when user is on the last page', () => {
+    it('it does not show link to next page', async () => {
+      axios.get.mockResolvedValue({ data: Array(15).fill({}) })
+      const queryParams = { page: '2' }
+      const $route = createRoute(queryParams)
+      renderJobListings($route)
+
+      await screen.findAllByRole('listitem')
+
+      const nextLink = screen.queryByRole('link', {
+        name: /next/i
+      })
+
+      expect(nextLink).not.toBeInTheDocument()
+    })
+
+    it('shows link to the previous page', async () => {
+      axios.get.mockResolvedValue({ data: Array(15).fill({}) })
+      const queryParams = { page: '2' }
+      const $route = createRoute(queryParams)
+      renderJobListings($route)
+
+      await screen.findAllByRole('listitem')
+
+      const previousLink = screen.queryByRole('link', {
+        name: /previous/i
+      })
+
+      expect(previousLink).toBeInTheDocument()
+    })
   })
 })
