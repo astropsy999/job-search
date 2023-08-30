2861c6585bcd0723a68576f9b62df9cfea157af5
astropsy999 <bareust@gmail.com>
2023-08-30 10:40:17 +0300

    18 creating mock backend

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -1,30 +1,35 @@
 <template>
   <li class="mb-7">
     <router-link
-      to="/jobs/results/1"
+      :to="jobPageLink"
       class="mx-auto block rounded border border-solid border-brand-gray-2 bg-white hover:shadow-gray"
     >
       <div class="mx-8 border-b border-solid border-brand-gray-2 pt-5 pb-2">
-        <h2 class="mb-2 text-2xl">Technical Program Manager, Perception, Augmented Reality</h2>
+        <h2 class="mb-2 text-2xl">{{ job.title }}</h2>
         <div class="flex flex-row align-middle">
-          <div class="mr-5"><span>Job</span></div>
-          <div><span>San Francisco, CA, USA</span></div>
+          <div class="mr-5">
+            <span>{{ job.organization }}</span>
+          </div>
+          <div>
+            <ul>
+              <li v-for="location in job.locations" :key="location" class="mr-5 inline-block">
+                <span>{{ location }}</span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <div class="px-8 py-4">
         <div><h3 class="mt-1 mb-2">Qualifications:</h3></div>
         <div>
           <ul class="list-disc pl-8">
-            <li>Bachelors's degree or equivalent practical experience.</li>
-            <li>5 years of experience in programm managment</li>
-            <li>
-              Experience analyzing data through queriying database(e.g. SQL) using spreadsheet
-              software and creating statistical models
+            <li v-for="qualification in job.minimumQualifications" :key="qualification">
+              {{ qualification }}
             </li>
           </ul>
         </div>
         <div class="mt-2 text-center">
-          <router-link to="/jobs/results/1" class="text-brand-blue-1">Expand</router-link>
+          <router-link :to="jobPageLink" class="text-brand-blue-1">Expand</router-link>
         </div>
       </div>
     </router-link>
@@ -33,7 +38,18 @@
 
 <script>
 export default {
-  name: 'JobListing'
+  name: 'JobListing',
+  props: {
+    job: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    jobPageLink() {
+      return `/jobs/results/${this.job.id}`
+    }
+  }
 }
 </script>
 
--- src/components/JobSearch/TheHero.vue
+++ src/components/JobSearch/TheHero.vue
@@ -3,7 +3,7 @@
     <section class="flex flex-col h-screen pt-10 pb-20">
       <div class="grid grid-cols-12">
         <div class="col-start-1 col-span-1"></div>
-        <div class="col-start-2 col-span-5"><TheHeadline /><JobSearchForm /></div>
+        <div class="col-start-2 col-span-5"><the-headline /><job-search-form /></div>
         <div class="col-start-7 col-span-5 self-center justify-self-center">
           <img
             class="h-80 w-80 object-contain"
